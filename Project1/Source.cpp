0afd110b125550af4d05fe1469328050d8bb51a8
DedMoroz132 <dedmoroz123132@gmail.com>
2020-09-27 15:50:35 +0700

    workig code

--- Project1/Source.cpp
+++ Project1/Source.cpp
@@ -1,7 +1,14 @@
 #include <stdio.h>
 #include <math.h>
+const double PI = 3.1415926;
 int Evklid(int a, int b)
 {
+	if (b > a)
+	{
+		int c = a;
+		a = b;
+		b = c;
+	}
 	int k = 1;
 	while (a != 0 && b != 0)
 	{
@@ -22,43 +29,50 @@ int Evklid(int a, int b)
 	}
 	return b * k;
 }
-int main(void) {
-	const double PI = 3.1415926;
-	int z, n,up,down,g,k,i=1;
-	double angle_pi, q=0,angle;
+double norm(int z, int n)
+{
+	double angle;
+	if (z > 0)
+	{
+		angle = atan(n / z) * 180 / PI;
+	}
+	else if (n > 0)
+	{
+		angle = 180 + atan(n / z) * 180 / PI;
+	}
+	else
+	{
+		angle = atan(n / z) * 180 / PI - 180;
+	}
+	if (angle < 0)
+	{
+		angle = angle + 360;
+	}
+	return angle;
+}
+int main()
+{
+	int z, n, up, down, g, k, i = 1, t, down1, up1, g1;
+	double angle_pi, q = 0, angle;
 	printf("Enter a and b:\n");
 	scanf_s("%d%d", &z, &n);
 	q = sqrt(z * z + n * n);
-	if (z == 0 || n==0)
+	if (z == 0 || n == 0)
 	{
 		if (n == 0)
 		{
 			printf("%lf\n", q);
 		}
 		else
-		if (z == 0)
-		{
-			printf("%lfi\n", q);
-		}
+			if (z == 0)
+			{
+				printf("%lfi\n", q);
+			}
+		t = 0;
 	}
 	else
 	{
-		if (z > 0)
-		{
-			angle = atan(n / z)*180/PI;
-		}
-		else if (n > 0)
-		{
-			angle = 180 + atan(n / z)*180/PI;
-		}
-		else
-		{
-			angle = atan(n / z)*180/PI - 180;
-		}
-		if (angle < 0)
-		{
-			angle = angle + 360;
-		}
+		angle = norm(z, n);
 		up = 1000000;
 		angle_pi = 180 / angle;
 		down = round(angle_pi * up);
@@ -66,35 +80,38 @@ int main(void) {
 		up = up / k;
 		down = down / k;
 		printf("%lf(cos PI %d/%d + i sin PI %d/%d)\n", q, up, down, up, down);
-		if (n < 0)
+		printf("%lf\n", (double)up / down);
+		t = 1;
+	}
+	if (t == 1)
+	{
+		if (n > 0)
 		{
-			i = -1;
-		}
-		if (i == 1)
-		{
-			printf("%lf - i %lf\n", z / q, n / q);
+			printf("%lf - i %lf\n", z / (q * q), n / (q * q));
 		}
 		else
 		{
-			printf("%lf + i %lf\n", z / q, n / q);
+			printf("%lf + i %lf\n", z / (q * q), fabs(n / (q * q)));
 		}
 		printf("enter the degree of the number:\n");
 		scanf_s("%d", &g);
 		k = Evklid(down, g);
-		down = down / k;
+		down1 = down / k;
 		g = g / k;
-		printf("%lf(cos PI %d/%d + i sin PI %d/%d)\n", powl(q, g), up * g, down, up * g, down);
+		printf("%lf(cos PI %d/%d + i sin PI %d/%d)\n", powl(q, g), up * g, down1, up * g, down1);
+		printf("%lf\n", ((double)up * g) / down1);
 		printf("Enter the degree of the root:\n");
 		scanf_s("%d", &g);
-		up = up * 1000000;
-		g = g * 1000000;
-		k = Evklid(up, g);
-		up = up / k;
-		g = g / k;
-		printf("%lf(cos PI %d/%d + i sin PI %d/%d)\n", powl(q, 1 / (double)g), up, down * g, up, down * g);
-		if (g % 2 == 0)
+		int n = g;
+		g = g;
+		for (i = 0; i < n; i++)
 		{
-			printf("-%lf(cos PI %d/%d + i sin PI %d/%d)\n", powl(q, 1 / (double)g), up, down * g, up, down * g);
+			up1 = (up + 2 * down * i);
+			k = Evklid(up1, g);
+			up1 = up1 / k;
+			g1 = g / k;
+			printf("%lf(cos PI %d/%d + i sin PI %d/%d)\n", powl(q, 1 / (double)g), up1, down * g1, up1, down * g1);
+			printf("%lf \n", (double)up1 / (down * g1));
 		}
 	}
 	return 0;
